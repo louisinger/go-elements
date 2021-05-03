08a6d5f6b5f9b724bd2ddec775a1386590c9fae8
louisinger <louis@vulpem.com>
2021-05-03 09:11:53 +0200

    types

--- wasm/main.go
+++ wasm/main.go
@@ -4,26 +4,28 @@ import (
 	"encoding/hex"
 	"syscall/js"
 
+	"github.com/btcsuite/btcd/btcec"
 	"github.com/vulpemventures/go-elements/pegin"
 )
 
 // Main function: it sets up our Wasm application
 func main() {
 	// Define the function in the JavaScript scope
-	js.Global().Set("getPeginAddress", GetPeginAddress())
+	js.Global().Set("getPeginAddress", GetPeginAddressWrapper())
 	// Prevent the function from returning, which is required in a wasm module
 	select {}
 }
 
-func GetPeginAddress() js.Func {
+// GetPeginAddressWrapper returns the javascript bind for pegin/GetAddressInfo function
+func GetPeginAddressWrapper() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		pubKey := h2b(args[0].String())
+		privKey, _ := btcec.PrivKeyFromBytes(btcec.S256(), h2b(args[0].String()))
 		fedPegInfo := args[1].String()
-		net := args[2].Int()
+		net := pegin.NetworkType(args[2].Int())
 		isDynaFed := args[3].Bool()
 		contract := h2b(args[4].String())
 		pegin.GetAddressInfo(
-			pubKey,
+			*privKey,
 			fedPegInfo,
 			net,
 			isDynaFed,
